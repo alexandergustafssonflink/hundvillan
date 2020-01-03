b20bc8d7ed58db9d44d8bbea76fd929b24ec7b3b
alexandergustafssonflink <50074799+alexandergustafssonflink@users.noreply.github.com>
2020-01-03 13:09:45 +0100

    Everything except like function of the basics seem to work

--- public/app/posts/update.php
+++ public/app/posts/update.php
@@ -18,36 +18,42 @@ if (isset($_POST['content']) || isset($_FILES['image'])) {
     $content = trim(filter_var($_POST['content'], FILTER_SANITIZE_STRING));
     $image = $_FILES['image'];
 
-    if (!($image['name'] == '')) {
-        $destination = '/app/uploads/images/';
+    if (!($image['name'] === '')) {
+        $destination = '/../uploads/images/';
         $fileName = $user['id'] . random_int(1, 999999) . '.jpeg';
         move_uploaded_file($image['tmp_name'], __DIR__ . $destination . $fileName);
         $avatarUrl = $destination . $fileName;
         $image['name'] = $fileName;
+        $sql = 'UPDATE posts SET content = :content, image = :image WHERE id = :id';
+
+        $statement = $pdo->prepare($sql);
+        if (!$statement) {
+            die(var_dump($pdo->errorInfo()));
+        }
+
+        $statement->bindParam(':content', $content, PDO::PARAM_STR);
+        $statement->bindParam(':id', $_GET['id'], PDO::PARAM_INT);
+        $statement->bindParam(':image', $image['name'], PDO::PARAM_STR);
+
+        $statement->execute();
     } else {
-        $image['name'] = NULL;
+        $sql = 'UPDATE posts SET content = :content WHERE id = :id';
+        $statement = $pdo->prepare($sql);
+        if (!$statement) {
+            die(var_dump($pdo->errorInfo()));
+        }
+
+        $statement->bindParam(':content', $content, PDO::PARAM_STR);
+        $statement->bindParam(':id', $_GET['id'], PDO::PARAM_INT);
+        $statement->execute();
     }
 
-    $sql = 'UPDATE posts SET content = :content, image = :image WHERE id = :id';
 
-    $statement = $pdo->prepare($sql);
-    if (!$statement) {
-        die(var_dump($pdo->errorInfo()));
-    }
-
-    $statement->bindParam(':content', $content, PDO::PARAM_STR);
-    $statement->bindParam(':id', $_GET['id'], PDO::PARAM_INT);
-    $statement->bindParam(':image', $image['name'], PDO::PARAM_STR);
-
-    $statement->execute();
 
     redirect(('/myprofile.php'));
 }
 
 
-
-
-// In this file we update posts in the database.
 
 if (isset($_GET['id'])) : ?>
     <?php $id = $_GET['id'] ?>
